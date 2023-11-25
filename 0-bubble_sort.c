1da95d6957ef1f48f9bb6b7467d670cd54be3021
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-11-25 15:05:34 +0200

    ..

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,7 +15,8 @@ void bubble_sort(int *array, size_t size)
 	{
 		for (j = 0; j < size - i; j++)
 		{
-			if (j + 1 > (size - i) - 1) break;
+			if (j + 1 > (size - i) - 1)
+				break;
 
 			if (array[j] > array[j + 1])
 			{
